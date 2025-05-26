0bfb8101e45b9940a3d87c7752d3577ede745dba
rossakyl <rossakayla01@gmail.com>
2025-05-26 19:23:25 +0700

    tambahkan anak ke objek ibu

--- agregrasi.cpp
+++ agregrasi.cpp
@@ -13,5 +13,10 @@ int main()
     anak *varAnak2 = new anak("rini");
     anak *varAnak3 = new anak("Dewi");
 
-   
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    
 }
