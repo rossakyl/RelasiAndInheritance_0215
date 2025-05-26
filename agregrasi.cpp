96031b07bafe8d54ade6092c043ea8fb5f73e51b
rossakyl <rossakayla01@gmail.com>
2025-05-26 18:48:42 +0700

    buat class ibu dengan atribut nama dan constructor

---
+++ agregrasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main()
+{
+    ibu *varIbu = new ibu("dini");
+    ibu *varibu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+}
--- ibu.h
+++ ibu.h
@@ -11,10 +11,7 @@ public:
     {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
-    ~ibu()
-    {
-        cout << "Ibu \"" << nama << "\" tidak ada\n";
-    }
-    void tambahAnak(anak *);
-    void cetakAnak();
+    
 };
+
+#endif
